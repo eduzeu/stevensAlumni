8efdf61be2adf5bb4962428c21a5b2fa0c90fdce
Daniel Fong <dkf2003@gmail.com>
2025-04-10 14:20:20 -0400

    Added new backend routes

--- server/app.js
+++ server/app.js
@@ -2,12 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import { users } from "./config/mongoCollections";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 let userCollection = await users();
 let saltrounds = 10;
 
 const app = express();
 
 app.use(express.json()); 
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // 'Bearer <token>'
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, 'secret_key', (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user; // Store the decoded user info in req
+    next();
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
@@ -21,19 +35,95 @@ app.post("/api/login", async (req, res) => {
   if(!user || compare) {
     return res.status(401).json({message: 'Invalid Login'});
   }
-  const token = jwt.sign({ id: user.id, name: user.name }, 'secret_key');
+  const token = jwt.sign({ _id: user._id, email: user.email }, 'secret_key');
   return res.json({ token });
 });
 
 app.post("/api/createAccount", async (req, res) => {
-  const {email, password} = req.body;
+  const {
+    email,
+    password,
+    alumniId,
+    fullName,
+    gradYear,
+    major,
+    jobTitle,
+    company,
+    linkedIn,
+    location,
+    mentoring,
+    donate
+    } = req.body;
   let alreadyExists = userCollection.findOne({email: email});
   if(alreadyExists){
     return res.status(400).json({message: 'User with this email already exists'});
   }
   let hashedPass = await bcrypt.hash(password);
-  let inserted = userCollection.insertOne({email: email, password: hashedPass});
+  let today = new Date();
+  today.setHours(0, 0, 0, 0);
+  let userObj = {
+    "Alumni Id": alumniId,
+    "Full Name": fullName,
+    "Graduation Year": gradYear,
+    "Major": major,
+    "Email": email,
+    "Job Title": jobTitle,
+    "Company": company,
+    "LinkedIn Profile": linkedIn,
+    "Location": location,
+    "Interested in Mentoring?": mentoring,
+    "Willing to Donate": donate,
+    "Last Updated": today,
+    "Password": hashedPass,
+    "Mentorees": [],
+    "Mentors": [],
+    "IncomingRequests": [],
+    "OutgoingRequests": []
+  }
+  let inserted = userCollection.insertOne(userObj);
+  if(!inserted.acknowledged){
+    return res.status(403).json({message: "Server error, could not create account"});
+  }
+  return res.json({message: "Account created successfully"});
 });
+
+app.get("/api/getAllMentors", async (req, res) => {
+  let allUsers = userCollection.find({"Interested in Mentoring?": true}).toArray();
+  return res.json({users: allUsers});
+});
+
+app.get("/api/getAllUsers", async (req, res) => {
+  let allUsers = userCollection.find({}).toArray();
+  return res.json({users: allUsers});
+});
+
+app.get("/api/getAllActiveMentors", async (req, res) => {
+  let mentors = userCollection.find({$expr: { $gte: [{ $size: "$Mentorees" }, 1]}}).toArray();
+  return res.json({users: mentors});
+});
+
+app.post("/api/acceptMentoree/:mentoreeId", authenticateToken, async (req, res) => {
+  let currUser = req.user;
+  const {mentoreeId} = req.params;
+  let user = await userCollection.findOne({_id: currUser._id});
+  user["IncomingRequests"] = user["IncomingRequests"].filter(request => request.toString() != mentoreeId);
+  user["Mentorees"].push(new ObjectId(mentoreeId));
+  let updated = await userCollection.findOneAndReplace({_id: currUser._id}, user);
+  if(!updated){
+    return res.status(403).json({message: "Error updating collection"});
+  }
+  let mentoree = await userCollection.findOne({_id: new ObejctId(mentoreeId)});
+  mentoree["OutgoingRequests"] = user["OutgoingRequests"].filter(request => request != currUser._id);
+  mentoree["Mentors"].push(currUser._id);
+  let updatedMentoree = await userCollection.findOneAndReplace({_id: new ObjectId(mentoreeId)}, mentoree);
+  if(!updatedMentoree){
+    return res.status(403).json({message: "Error updating collection"});
+  }
+  return res.json({message: "Update Ok"});
+});
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
